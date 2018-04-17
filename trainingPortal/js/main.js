be37b2b317e7fa6d4d60cc57defad3288d08da9b
Kert Ojasaar <kertojasaar@gmail.com>
2018-04-17 19:00:46 +0300

    Registering front-end added.

--- trainingPortal/js/main.js
+++ trainingPortal/js/main.js
@@ -1,4 +1,22 @@
 $('#login-btn').click(function() {
   const username = $('#username').val();
   const password = $('#password').val();
+  console.log('login');
 });
+
+function register() {
+  $('.error').css('display', 'none');
+  const username = $('#username').val();
+  const password = $('#password').val();
+  const password2 = $('#password2').val();
+  if (password.length < 8) {
+    console.log('Pw too short');
+    $('#wrong-pw-error').css('display', 'block');
+  } else if (password !== password2) {
+    console.log('Invalid pw');
+    $('#diff-pw-error').css('display', 'block');
+  } else {
+    // make register request.
+    console.log('Success!');
+  }
+}
